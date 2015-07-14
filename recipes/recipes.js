63269dd336f34c45e9f8fb63478f88d7bc7a11cc
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-07-14 22:52:37 +0000

    remove kaltura recipe subdir

--- recipes/recipes.js
+++ recipes/recipes.js
@@ -3,11 +3,13 @@ var Recipes = module.exports = {};
 var FS = require('fs');
 var Path = require('path');
 
-var files = FS.readdirSync(__dirname + '/kaltura');
+var files = FS.readdirSync(__dirname);
+var thisFile = Path.basename(__filename);
 files.forEach(function(filename) {
+  if (filename === thisFile) return;
   var name = filename.substring(0, filename.length - 5);
   try {
-    Recipes[name] = JSON.parse(FS.readFileSync(Path.join(__dirname, 'kaltura', filename), 'utf8'));
+    Recipes[name] = JSON.parse(FS.readFileSync(Path.join(__dirname, filename), 'utf8'));
   } catch (e) {
     console.log('Error parsing recipe ' + name);
     throw e;
