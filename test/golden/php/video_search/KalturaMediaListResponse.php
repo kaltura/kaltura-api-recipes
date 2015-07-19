655322346df2a5878f304146798d03e5fa17b2bf
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-07-19 19:29:10 +0000

    fix video_search

--- test/golden/php/video_search/KalturaMediaListResponse.php
+++ test/golden/php/video_search/KalturaMediaListResponse.php
@@ -1,8 +1,5 @@
 <h1>Videos</h1>
 <hr></hr>
-<?php if($result->length == 0) { ?>
-  <p><i>No results</i></p>
-<?php } ?>
 <?php foreach($result as $index=>$video) { ?>
   <?php if($index < 5) { ?>
     <?php
