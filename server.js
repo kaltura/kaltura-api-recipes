1a9bd9ec76843e0830205946f0148821307b58d5
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-07-21 22:56:35 +0000

    Allow insecure port in devlopment, add bower dep

--- server.js
+++ server.js
@@ -10,18 +10,21 @@ App.use('/', Express.static(__dirname + '/static'));
 App.use('/', require('./routes/pages.js'));
 App.use('/recipes', require('./routes/recipes.js'));
 
-//App.listen(process.env.KALTURA_RECIPES_PORT || 3000);
-var port=process.env.KALTURA_RECIPES_PORT || 443
-var https = require('https'),
-fs = require('fs');
-var sslOptions = {
-  key: fs.readFileSync('/etc/ssl/certs/kaltura.org.key'),
-  cert: fs.readFileSync('/etc/ssl/certs/kaltura.org.crt'),
-  ca: fs.readFileSync('/etc/ssl/certs/ca-kaltura.org.crt'),
-  requestCert: true,
-  rejectUnauthorized: false
-};
-var secureServer = https.createServer(sslOptions,App).listen(port, function(){
-  console.log("Secure Express server listening on port "+port);
-});
-
+if (process.env.LUCYBOT_DEV) {
+  console.log('----DEVELOPMENT ENVIRONMENT----');
+  App.listen(process.env.KALTURA_RECIPES_PORT || 3000);
+} else {
+  var port=process.env.KALTURA_RECIPES_PORT || 443
+  var https = require('https'),
+  fs = require('fs');
+  var sslOptions = {
+    key: fs.readFileSync('/etc/ssl/certs/kaltura.org.key'),
+    cert: fs.readFileSync('/etc/ssl/certs/kaltura.org.crt'),
+    ca: fs.readFileSync('/etc/ssl/certs/ca-kaltura.org.crt'),
+    requestCert: true,
+    rejectUnauthorized: false
+  };
+  var secureServer = https.createServer(sslOptions,App).listen(port, function(){
+    console.log("Secure Express server listening on port "+port);
+  });
+}
