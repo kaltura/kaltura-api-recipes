c0d87873158f49c38030562e734fd4c769efa5a9
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-07-27 17:05:38 +0000

    refactor ad cue point recipe

---
+++ test/golden/php/ad_cue_points/AddAdCuePoint.php
@@ -0,0 +1,20 @@
+<?php
+  include "AddAdCuePointForm.php";
+?>
+<script>
+  $('#AddAdCuePointForm').submit(function() {
+    var data = new FormData(document.getElementById('AddAdCuePointForm'));
+     $.ajax({
+       url: '/addCuePoint.php',
+       type: 'POST',
+       data: data,
+       contentType: false,
+       cache: false,
+       processData: false,
+       success: function (data, textStatus, jqXHR) {
+         $('.container').load('listCuePoint.php', JSON.parse(data));
+       }
+     });
+    return false;
+  })
+</script>
