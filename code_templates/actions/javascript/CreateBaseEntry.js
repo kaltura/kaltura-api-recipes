ff6bbc8104de301da1e8a08758fa50c608ee3d66
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-07-28 15:21:41 +0000

    Start changing flow

--- code_templates/actions/javascript/CreateBaseEntry.js
+++ code_templates/actions/javascript/CreateBaseEntry.js
@@ -1,22 +1,29 @@
 entry = new KalturaMediaEntry();
 entry.name = <%- Lucy.code.variable('answers.name') %>;
-var uploadTokenId = <%- Lucy.code.variable('answers.uploadTokenId') %>;
-
-var checkStatus = function(success, results) {
-  if (!success || (results.code && results.message)) {
-    console.log('Kaltura Error', success, results);
-  } else if (results.status !== 2) {
-    var statusMessage = results.status === 1 ?
-        'converting' : 'status ' + results.status;
-    $('#UploadStatus').text(
-        'Entry not ready yet (' + statusMessage + ')');
-    setTimeout(function() {
-      client.media.get(checkStatus, results.id);
-    }, 100);
-  } else {
-    $('#UploadStatusBar').hide();
-    $('#UploadSuccess').show();
-    <%- Lucy.returnCode('results') %>
+entry.type = KalturaEntryType.AUTOMATIC;
+client.media.add(function(success, entry) {
+  if (!success || (entry.code && entry.message)) {
+    return console.log('Kaltura Error', success, entry);
   }
-}
-client.baseEntry.addFromUploadedFile(checkStatus, entry, uploadTokenId);
+  var mediaResource = new KalturaUploadedFileTokenResource();
+  mediaResource.token = <%- Lucy.code.variable('answers.uploadTokenId') %>;
+  var checkStatus = function(success, results) {
+    if (!success || (results.code && results.message)) {
+      console.log('Kaltura Error', success, results);
+    } else if (results.status !== 2) {
+      var statusMessage = results.status === 1 ?
+          'converting' : 'status ' + results.status;
+      $('#UploadStatus').text(
+          'Entry not ready yet (' + statusMessage + ')');
+      setTimeout(function() {
+        client.media.get(checkStatus, results.id);
+      }, 1000);
+    } else {
+      $('#UploadStatusBar').hide();
+      $('#UploadSuccess').show();
+      <%- Lucy.returnCode('results') %>
+    }
+  }
+  client.media.addContent(checkStatus, entry.id, mediaResource);
+}, entry);
+
