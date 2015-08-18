c6e1afda3d16662860963f28edbd9facb97f1456
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-08-18 19:02:16 +0000

    fix createMediaEntry for ruby

--- code_templates/actions/ruby/CreateMediaEntry.rb
+++ code_templates/actions/ruby/CreateMediaEntry.rb
@@ -1,7 +1,7 @@
 entry = KalturaMediaEntry.new()
 entry.name = <%- Lucy.code.variable('answers.name') %>
 entry.media_type = <%- Lucy.code.variable('answers.mediaType') %>
-entry = @@client.media.add(entry)
+entry = @@client.media_service.add(entry)
 mediaResource = KalturaUploadedFileTokenResource.new()
 mediaResource.token = <%- Lucy.code.variable('answers.uploadTokenId') %>
 entry = @@client.media_service.add_content(entry.id, mediaResource)
