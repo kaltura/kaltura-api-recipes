667fde7cbb35618203635dc5115dd5009eaf4bdd
Jess Portnoy <jess.portnoy@kaltura.com>
2015-09-02 18:18:09 +0100

    and now with the golden tests...

---
+++ test/golden/php/webvtt_captions/p1/getCaptionAsset.php
@@ -0,0 +1,31 @@
+<?php
+require_once('../../lib/KalturaClient.php');
+require_once('Credentials.php');
+
+$config = new KalturaConfiguration(PARTNER_ID);
+$config->serviceUrl = "https://www.kaltura.com/";
+$client = new KalturaClient($config);
+$ks = $client->session->start(
+  SECRET,
+  USER_ID,
+  SESSION_TYPE,
+  PARTNER_ID,
+  null, null);
+$client->setKS($ks);
+
+$captionAssetId = null;
+
+try {
+  $result = $client->captionAsset->get(
+    $captionAssetId);
+  $result = (object)$result;
+  require 'KalturaCaptionAsset.php';
+} catch (Exception $e) {
+  $result = array(
+    code => $e->getCode(),
+    message => $e->getMessage()
+  );
+  $result = (object)$result;
+  require 'KalturaCaptionAsset.php';
+}
+?>
