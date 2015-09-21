7f5655dfb0da872780c6615cc1630dd512bb25d4
Jess Portnoy <jess.portnoy@kaltura.com>
2015-09-21 18:02:53 +0100

    metadata recipe in progress

--- recipes/metadata.js
+++ recipes/metadata.js
@@ -25,7 +25,7 @@ module.exports = {
           "name": "systemName"
         },
         {
-          "default": "1",
+          "default": 1,
           "type": "number",
           "name": "metadataObjectType",
           "hidden": true
@@ -43,31 +43,101 @@ module.exports = {
           "hidden": true
         },
         {
-	  "default": FS.readFileSync(__dirname + '/data/metadata_profile_sample.xsd', 'utf8'),
+          "default": FS.readFileSync(__dirname + '/data/metadata_profile_sample.xsd', 'utf8'),
           "type": "text",
           "label": "XSD data",
           "name": "xsdData",
           "hidden": true
         }
      ]  
-    }
-    ],
+    }, 
+    {
+      "page": 1,
+      "title": "Add value",
+      "affects": "addMetadata",
+      "tip": ".",
+      "disableAutorefresh": true,
+      "inputs": [
+        {
+          "default": "",
+          "type": "text",
+          "label": "entryId",
+          "name": "objectId"
+        },
+        {
+          "default": "",
+          "type": "text",
+          "label": "XML",
+          "name": "xmlData"
+        },
+        {
+          "default": 1,
+          "type": "number",
+          "name": "objectType",
+          "hidden": true
+        },
+	{
+	 "type": "select-chosen",
+	 "name": "Metadata Profile",
+	 "dynamicChoices": {
+	    "service": "metadataProfile",
+	    "action": "list",
+		"map": {
+		    "value": "id",
+		    "label": "name"
+		}
+	    }
+	}
+     ]  
+    },
+    {
+      "page": 2,
+      "inputs": [],
+    }],
     "pages": [
         {
             "view": "metadataProfileShow",
             "data": {
                 "action": "addMetadataProfile"
+            },
+            "view": "metadataShow",
+            "data": {
+                "action": "addMetadata"
             }
+        },
+        {
+          "view": "KalturaMetadataProfileListResponse",
+          "data": {
+            "action": "listMetadataProfile"
+          }
         }
     ],
     "views": [
         "metadataProfileShow",
+        "metadataShow",
+        "KalturaMetadataProfileListResponse",
+        "metadataProfileDeleted",
     ],
     "actions": [
         {
             "service": "metadataProfile",
             "action": "add",
             "view": "metadataProfileShow"
+        },
+        {
+            "service": "metadata",
+            "action": "add",
+            "view": "metadataShow"
+        },
+        {
+            "service": "metadataProfile",
+            "action": "deleteAction",
+            "view": "metadataProfileDeleted"
+        },
+        {
+            "service": "metadataProfile",
+            "action": "listAction",
+            "view": "KalturaMetadataProfileListResponse"
         },
      ]
 }
