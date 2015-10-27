3ff450686515a6c0d0b94d40761559b9ae0dc037
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-10-27 18:19:02 +0000

    Update PHP styles after lucy-codegen change

--- test/golden/php/captions/p1/CaptionsAttached.php
+++
@@ -1,9 +0,0 @@
-<p>Captions attached!</p>
-<div class="KalturaMediaEntry"></div>
-<script>
-  var element = $('.KalturaMediaEntry').last();
-  element[0].loadData = function() {
-    $('.KalturaMediaEntry').last().load('getMedia.php');
-  }
-  element[0].loadData();
-</script>
