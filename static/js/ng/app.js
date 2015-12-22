7e48833772f717e79942d1b5e3dd4faa156179ca
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2015-12-22 15:29:14 +0000

    add suggestion box for empty search results

--- static/js/ng/app.js
+++ static/js/ng/app.js
@@ -1,5 +1,18 @@
 var App = angular.module('App', ['localytics.directives']);
+
 App.controller('Recipes', function($scope) {
   $scope.recipes = RECIPES;
 })
 
+App.controller('Suggestion', function($scope) {
+  $scope.sendSuggestion = function() {
+    mixpanel.track('suggestion', {
+      text: $scope.suggestion,
+    })
+    $scope.success = true;
+    setTimeout(function() {
+      $scope.success = false;
+      $scope.$apply();
+    }, 2000)
+  }
+})
