e2490a3156f6be81b61ef04f68cc6a222ed2435c
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2015-12-30 17:32:55 +0000

    add package manager information

--- static/js/download-info.js
+++ static/js/download-info.js
@@ -1,5 +1,20 @@
 var clientLibLink = 'https://www.kaltura.com/api_v3/testme/client-libs.php';
 
+window.getPackageInfo = function(language, recipe) {
+  var message = "<p>You can also install the client library ";
+  if (language.id === 'node') {
+    message += "via npm: <code>npm install kaltura</code>";
+  } else if (language.id === 'ruby') {
+    message += "as a Gem: <code>gem install kaltura-client</code>";
+  } else if (language.id === 'php') {
+    message += "via composer: <code>composer require kaltura/api-client-library</code>";
+  } else {
+    return;
+  }
+  message += "</p><p class='small'>Be sure to change import/require statements where appropriate.</p>";
+  return message;
+}
+
 window.getDownloadInfo =  function(language, recipe) {
   var libDir = language.id === 'javascript' ? '/js' : '/lib';
   var message =
@@ -8,11 +23,11 @@ window.getDownloadInfo =  function(language, recipe) {
   + '    Visit <a href="' + clientLibLink + '" target="_blank">' + clientLibLink + '</a>'
   + '    to download the library, and extract it to the ' + libDir + ' directory inside this zip.'
   + '  </p>';
+  message += getPackageInfo(language, recipe) || '';
   return message;
 }
 
 window.getDownloadFiles = function(language, recipe, callback) {
-  console.log('get dlf');
   var libDir = language.id === 'javascript' ? 'js' : 'lib';
   var readme = [
     '# Kaltura ' + recipe.title + ' Sample App',
