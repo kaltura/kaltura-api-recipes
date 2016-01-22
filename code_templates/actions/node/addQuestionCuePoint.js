cfa13dffca591f9a4b41f2b82c6e7ba81c1d4654
Jess Portnoy <jess.portnoy@kaltura.com>
2016-01-22 12:55:58 +0000

    nodejs quiz implementation.

--- code_templates/actions/node/addQuestionCuePoint.js
+++ code_templates/actions/node/addQuestionCuePoint.js
@@ -1,19 +1,35 @@
-console.log('here I am');
+var entryId = <%- Lucy.code.variable('answers.entryIdEqual') %>;
 var quiz = new KalturaQuiz();
-console.log(quiz);
-var attsAr = [];
-attsAr.push({'key' : WelcomeMessage , 'value' : 'blah'});
-attsAr.push({'key' : inVideoTip , 'value' : true});
-attsAr.push({'key' : canSkip , 'value' : true});
-quiz.uiAttributes = attsAr;
-client.quiz.add(function(result) {
-  <%- Lucy.returnCode('result') %>
-}, <%- Lucy.code.variable('answers.entryIdEqual') %>,quiz);
+var filter = new KalturaQuizFilter();
+filter.entryIdEqual = entryId;
+var pager = null;
+var result = client.quiz.listAction(function(success,result) {
+    if (result.objects.length === 0){
+	var attsAr = [];
+	attsAr.push({'key' : 'WelcomeMessage' , 'value' : 'blah'});
+	attsAr.push({'key' : 'inVideoTip' , 'value' : true});
+	attsAr.push({'key' : 'canSkip' , 'value' : true});
+	quiz.uiAttributes = attsAr;
+	client.quiz.add(function(result) {
+	},entryId,quiz);
+    }
 
-var cuePoint = new Kaltura.objects.KalturaQuestionCuePoint();
-cuePoint.quesion = req.body.question;
+}, filter, pager);
+
+
+var cuePoint = new KalturaQuestionCuePoint();
+cuePoint.entryId = entryId;
+cuePoint.question = req.body.question; 
 cuePoint.startTime = req.body.startTime;
-cuePoint.entryId = <%- Lucy.code.variable('answers.entryIdEqual') %>;
+var optionalAnswers =  [];
+optionalAnswers[0] =  [];
+optionalAnswers[1] =  [];
+
+optionalAnswers[0]={'key': 1, 'text': req.body.answer0, 'isCorrect': 1};
+
+optionalAnswers[1]={'key': 2, 'text': req.body.answer1, 'isCorrect': 0};
+console.log(optionalAnswers);
+cuePoint.optionalAnswers = optionalAnswers;
 client.cuePoint.add(function(result) {
-  <%- Lucy.returnCode('result') %>
+<%- Lucy.returnCode('result') %>
 }, cuePoint);
