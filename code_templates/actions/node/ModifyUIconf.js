fe5e2661a3fd87932fa0bab65c0d73f8383af59a
Jess Portnoy <jess.portnoy@kaltura.com>
2016-02-10 13:44:09 +0000

    Ruby implementation of player_uiconf

--- code_templates/actions/node/ModifyUIconf.js
+++ code_templates/actions/node/ModifyUIconf.js
@@ -1,5 +1,5 @@
 var id = <%- Lucy.code.variable('answers.id') %>;
-var uiConf = new KalturaUiConf();
+var uiConf = new Kaltura.objects.KalturaUiConf();
 uiConf.config=req.body.config;
 client.uiConf.update(function(result) {
 <%- Lucy.returnCode('result') %>
