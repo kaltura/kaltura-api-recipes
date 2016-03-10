475c9c37b46c8b70128c29ec69edf2ea4ca457bc
Bobby Brennan <bobby.brennan@gmail.com>
2016-03-10 21:50:12 +0000

    start processing recipes

---
+++ recipes-v2.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+
+var recipes = module.exports = {};
+
+var DIR = __dirname + '/recipes-v2';
+
+var XSD_DATA = FS.readFileSync(DIR + '/extras/metadata_profile_sample.xsd', 'utf8');
+
+fs.readdirSync(DIR).forEach(function(r) {
+  var filename = DIR + '/' + r;
+  if (FS.statSync(filename).isDirectory()) return;
+  var name = r.match(/^(.*)\./)[1];
+  recipes[name] = require(filename);
+  recipe.defaults = recipe.defaults || {};
+  recipe.defaults.serviceURL =
+      process.env.KALTURA_SERVICE_URL || 'https://www.kaltura.com/';
+  recipe.defaults.recipeName = recipe.name;
+
+  var opsUsed = [];
+  recipe.steps.forEach(function(step) {
+    if (!step.apiCall) return;
+    var match = step.apiCall.path.match(/\/service\/(.*)\/action\/(.*)$/);
+    opsUsed.push({service: match[1], action: match[2]});
+  })
+  if (opsUsed.length) {
+    recipe.description += '\n\nThis recipe uses the following operations:\n' + opsUsed.map(function(op) {
+      return '* ' + op.service + '.' + op.action;
+    }).join('\n');
+
+    recipe.finishText = [
+      '### Learn More',
+      'You can learn more about the services and actions used in this recipe by visiting the [API Console](/console)',
+      '',
+    ].concat(opsUsed.map(function(op) {
+      var url = '/service/' + op.service + '/action/' + op.action;
+      url = '/GET' + url;
+      var docUrl = '/console#/Documentation' + url;
+      var conUrl = '/console#/Console' + url;
+      return '* `' + op.service + '.' + op.action +
+          '` - [Documentation](' + docUrl + '), [Test Console](' + conUrl + ')';
+    })).join('\n');
+  }
+
+  if (recipe.name === 'metadata') recipe.defaults.xsdData = XSD_DATA;
+})
