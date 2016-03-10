a493b89c9394e656db5abf90a9036cdb1478ee0b
Bobby Brennan <bobby.brennan@gmail.com>
2016-03-10 22:08:42 +0000

    start converting api calls

--- scripts/convert-to-v2.js
+++ scripts/convert-to-v2.js
@@ -1,14 +1,17 @@
 var fs = require('fs');
+var schema = require('kaltura-schema');
 
-var V1_DIR = __dirname + '/../recipes'
-var V2_DIR = __dirname + '/../recipes-v2'
+var V1_DIR = __dirname + '/../recipes';
+var V2_DIR = __dirname + '/../recipes-v2';
 
-fs.readdirSync(V1_DIR).forEach(function(r) {
-  if (r === 'recipes.js') return;
-  if (fs.statSync(V1_DIR + '/' + r).isDirectory()) return;
-  var recipe = require(V1_DIR + '/' + r);
-  var newRecipe = convertRecipe(recipe)
-  fs.writeFileSync(V2_DIR + '/' + r.replace(/\.js$/, '.json'), JSON.stringify(newRecipe, null, 2))
+schema.initialize(function() {
+  fs.readdirSync(V1_DIR).forEach(function(r) {
+    if (r === 'recipes.js') return;
+    if (fs.statSync(V1_DIR + '/' + r).isDirectory()) return;
+    var recipe = require(V1_DIR + '/' + r);
+    var newRecipe = convertRecipe(recipe)
+    fs.writeFileSync(V2_DIR + '/' + r.replace(/\.js$/, '.json'), JSON.stringify(newRecipe, null, 2))
+  })
 })
 
 function convertRecipe(recipe) {
@@ -22,17 +25,36 @@ function convertRecipe(recipe) {
   newRecipe.icon = recipe.icon;
   newRecipe.tags = recipe.tags;
   newRecipe.keywords = recipe.keywords;
-  newRecipe.steps = recipe.recipe_steps.map(convertStep);
+  newRecipe.steps = recipe.recipe_steps.map(r => convertStep(r, recipe.actions));
 
   return newRecipe;
 }
 
-function convertStep(step) {
+function convertStep(step, actions) {
   var newStep = {};
 
   newStep.title = step.title;
   newStep.description = step.tip;
   newStep.parameters = step.inputs.map(convertInput);
+
+  (actions || []).forEach(function(a) {
+    var action = a.action.replace(/Action$/, '');
+    var service = a.service;
+    if (!service) {
+      var match = action.match(/^(get|list|add)(\w+)$/);
+      if (!match) return console.log('no match for action', action)
+      action = match[1];
+      service = match[2];
+    }
+    var comboName = action + service.charAt(0).toUpperCase() + service.substring(1);
+    if (step.code_snippet === comboName) {
+      service = schema.services[a.service].id;
+      newStep.apiCall = {
+        path: '/service/' + service + '/action/' + action,
+        method: 'get',
+      }
+    }
+  })
 
   return newStep;
 }
