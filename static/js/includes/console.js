1f699d28fd500bc282e6e4e7dffb92e65506fd5e
Bobby Brennan <bobby.brennan@gmail.com>
2016-03-11 20:26:29 +0000

    fix up checkResponse

--- static/js/includes/console.js
+++ static/js/includes/console.js
@@ -18,12 +18,13 @@ var startKS = function() {
 startKS();
 
 window.checkResponse = function(data, status, xhr) {
+  if (data === null) return {type: 'danger', message: 'Status Code ' + xhr.status + ': ' + xhr.responseText};
   if (data instanceof Document) {
     var err = $(data).find('error').length;
     if (err) return {type: 'danger', message: $(data).find('error message').text()};
   } else if (typeof data === 'object') {
-    var err = data.result.error;
-    if (err) return {type: 'danger', message: err.message};
+    var err = data.code && data.message;
+    if (err) return {type: 'danger', message: data.code + ': ' + data.message};
   }
   return {type: 'success', message: 'Success'}
 }
