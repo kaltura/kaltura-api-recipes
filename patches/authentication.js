022e755bb6f02036398584a1c44fd16ad1e16405
Bobby Brennan <bobby.brennan@gmail.com>
2016-03-21 23:00:49 +0000

    auth recipe work

--- patches/authentication.js
+++ patches/authentication.js
@@ -11,4 +11,31 @@ module.exports = function(recipe) {
     method: 'get',
   }
   delete recipe.steps[0].codeSnippet;
+
+  recipe.steps.push({
+    title: 'Logging in with your Login ID',
+    description: [
+      "You can also log in with your Login ID and password by using `user.loginByLoginId`. This will return a new KS."
+    ],
+    apiCall: {
+      path: '/service/user/action/loginByLoginId',
+      method: 'get',
+    },
+    parameters: [
+      {name: 'loginId'},
+      {name: 'password', 'x-inputType': 'password'},
+    ]
+  });
+
+  recipe.steps.push({
+    title: "Log In with a Partner ID",
+    description: [
+      "After using `user.loginByLoginId`, you can get a list of all your Partner IDs for that account using `partner.listPartnersForUser`.",
+      "You can then re-use `user.loginById` with a partnerId to login for as that particular partner.",
+    ],
+    apiCall: {
+      path: '/service/partner/action/listPartnersForUser',
+      method: 'get',
+    },
+  });
 }
