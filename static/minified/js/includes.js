5bb545972aeddb9296f33f996d523eaf99501119
Bobby Brennan <bobby.brennan@gmail.com>
2016-03-21 17:33:51 +0000

    show auth modal on error

--- static/minified/js/includes.js
+++ static/minified/js/includes.js
@@ -727,13 +727,17 @@ window.onAuthorization = function(creds, cb) {
 
 ;
 window.checkResponse = function(data, status, xhr) {
-  if (data === null) return {type: 'success', message: "Success"};
+  var msg = {type: 'success', message: "Success"};
+  if (data === null) return msg;
   if (data instanceof Document) {
     var err = $(data).find('error').length;
-    if (err) return {type: 'danger', message: $(data).find('error message').text()};
+    if (err) msg = {type: 'danger', message: $(data).find('error message').text()};
   } else if (typeof data === 'object') {
     var err = data.code && data.message;
-    if (err) return {type: 'danger', message: data.code + ': ' + data.message};
+    if (err) msg = {type: 'danger', message: data.code + ': ' + data.message};
   }
-  return {type: 'success', message: 'Success'}
+  if (msg.type !== 'success') {
+    $('#KalturaLogin').modal('show');
+  }
+  return msg;
 }
