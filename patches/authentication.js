52e2bda35a5393e14f8d52514894d209e955f745
Bobby Brennan <bobby.brennan@gmail.com>
2016-03-21 17:57:10 +0000

    fix auth description

--- patches/authentication.js
+++ patches/authentication.js
@@ -1,4 +1,11 @@
 module.exports = function(recipe) {
+  recipe.steps[0].description = [
+    "This recipe shows how to work with Kaltura Sessions (aka `KS`).  To generate a" +
+        " KS, you will need your API secret keys.",
+    "To retrieve these secret keys, visit the \"Integration Settings\" tab inside the" +
+        " [Kaltura Management Console](http://kmc.kaltura.com/index.php/kmc/kmc4#account|integration).",
+    "To sign up for a Kaltura VPaaS account, visit [vpaas.kaltura.com](https://vpaas.kaltura.com)",
+  ];
   recipe.steps[0].apiCall = {
     path: '/service/session/action/start',
     method: 'get',
