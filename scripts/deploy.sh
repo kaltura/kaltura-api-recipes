24c08e8defa13f193b8c6f35d4413a81525f117f
Bobby Brennan <bobby.brennan@gmail.com>
2016-04-19 15:52:16 +0000

    fix up deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,5 @@ export KALTURA_RECIPES_PORT=3005
 export DEVELOPMENT=
 export ENABLE_EDITS=true
 
-git pull && npm install && forever restart `pwd`/server.js 
+git pull && npm install
+forever restart `pwd`/server.js || forever start `pwd`/server.js
