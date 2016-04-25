90b0f7ba819c6ef39f9e5fd9dbc0d7f544dae8b9
Bobby Brennan <bobby.brennan@gmail.com>
2016-04-25 21:00:22 +0000

    add loadRecipe

--- static/minified/js/includes.js
+++ static/minified/js/includes.js
@@ -777,37 +777,70 @@ window.checkResponse = function(data, status, xhr) {
   return msg;
 }
 
-window.saveRecipe = function(recipe, callback) {
-  var token = '';
-  var github = function(method, path, query, data, innerCb) {
-    if (!innerCb) {
-      innerCb = data;
-      data = null;
+var GitHub = function (opts) {
+  this.o = opts;
+}
+GitHub.prototype.req = function(method, path, query, data, innerCb) {
+  var self = this;
+  if (!innerCb) {
+    innerCb = data;
+    data = null;
+  }
+  query = query || {};
+  headers = {'Content-Type': 'application/json'};
+  if (self.o.token) query.access_token = self.o.token;
+  $.ajax({
+    method: method,
+    url: 'https://api.github.com' + path + '?' + $.param(query),
+    data: data ? JSON.stringify(data) : undefined,
+    headers: headers,
+  }).success(innerCb).fail(function(xhr) {
+    if (xhr.responseJSON && xhr.responseJSON.errors && xhr.responseJSON.errors[0]) {
+      var err = xhr.responseJSON.errors[0];
+      if (err && err.message.indexOf('A pull request already exists') === 0) return innerCb({});
     }
-    query = query || {};
-    headers = {'Content-Type': 'application/json'};
-    if (token) query.access_token = token;
+    if (self.o.errBack) self.o.errBack(xhr.responseText);
+  });
+}
+
+function runGitHubOAuth(callback) {
+  window.onOAuthComplete = function(qs) {
     $.ajax({
-      method: method,
-      url: 'https://api.github.com' + path + '?' + $.param(query),
-      data: data ? JSON.stringify(data) : undefined,
-      headers: headers,
-    }).success(innerCb).fail(function(xhr) {
-      if (xhr.responseJSON && xhr.responseJSON.errors && xhr.responseJSON.errors[0]) {
-        var err = xhr.responseJSON.errors[0];
-        if (err && err.message.indexOf('A pull request already exists') === 0) return innerCb({});
-      }
+      method: 'POST',
+      url: '/github/access_token?code=' + qs.code,
+      headers: {'Content-Type': 'application/json'},
+    }).success(function(data) {
+      callback(null, data.access_token);
+    }).fail(function(xhr) {
       callback(xhr.responseText);
     });
-  }
-  var repoPath = '/repos/kaltura/kaltura-api-recipes';
-  var filePath = '/contents/recipes-v2/' + recipe.name + '.json';
+  };
   var params = {
     client_id: ENV.github_client_id,
     redirect_uri: ENV.github_callback_url,
     scope: 'public_repo',
   }
+  window.open('https://github.com/login/oauth/authorize?' + $.param(params), '_blank');
+}
 
+window.loadRecipe = function(callback) {
+  runGitHubOAuth(function(err, tok) {
+    if (err) return callback(err);
+    var github = new GitHub({errBack: callback, token: tok});
+    github.req('GET', '/user', {}, function(user) {
+      var path = '/repos/' + user.login + '/kaltura-api-recipes';
+      path += '/contents/recipes-v2/' + RECIPE.name + '.json';
+      github.req('GET', path, {}, function(file) {
+        var cont = atob(file.content);
+        callback(null, JSON.parse(cont));
+      });
+    });
+  })
+}
+
+window.saveRecipe = function(recipe, callback) {
+  var repoPath = '/repos/kaltura/kaltura-api-recipes';
+  var filePath = '/contents/recipes-v2/' + recipe.name + '.json';
   var descLoc = (recipe.description || '').indexOf('This recipe uses the following operations');
   if (descLoc === 0) delete recipe.description;
   else if (descLoc !== -1) recipe.description = recipe.description.substring(0, descLoc).split('\n\n');
@@ -823,34 +856,26 @@ window.saveRecipe = function(recipe, callback) {
     else s.description = s.description.split('\n\n');
   });
 
-  window.onOAuthComplete = function(qs) {
-    $.ajax({
-      method: 'POST',
-      url: '/github/access_token?code=' + qs.code,
-      headers: {'Content-Type': 'application/json'},
-    }).success(function(data) {
-      token = data.access_token;
-      github('POST', repoPath + '/forks', {}, function(repo) {
-        var theirRepoPath = '/repos/' + repo.full_name;
-        github('GET', theirRepoPath + filePath, {}, function(file) {
-          github('PUT', theirRepoPath + filePath, {}, {
-            message: "Update recipe: " + recipe.name,
-            content: btoa(unescape(encodeURIComponent(JSON.stringify(recipe, null, 2)))),
-            sha: file.sha,
-          }, function(msg) {
-            github('POST', repoPath + '/pulls', {}, {
-              title: 'Update recipe: ' + recipe.name,
-              base: 'development',
-              head: repo.owner.login + ':development',
-            }, function(pr) {
-              callback(null, 'Successfully opened a Pull Request' + (pr.html_url ? ' at ' + pr.html_url : ''));
-            })
-          });
+  runGitHubOAuth(function(err, tok) {
+    if (err) return callback(err);
+    var github = new GitHub({errBack: callback, token: tok});
+    github.req('POST', repoPath + '/forks', {}, function(repo) {
+      var theirRepoPath = '/repos/' + repo.full_name;
+      github.req('GET', theirRepoPath + filePath, {}, function(file) {
+        github.req('PUT', theirRepoPath + filePath, {}, {
+          message: "Update recipe: " + recipe.name,
+          content: btoa(unescape(encodeURIComponent(JSON.stringify(recipe, null, 2)))),
+          sha: file.sha,
+        }, function(msg) {
+          github.req('POST', repoPath + '/pulls', {}, {
+            title: 'Update recipe: ' + recipe.name,
+            base: 'development',
+            head: repo.owner.login + ':development',
+          }, function(pr) {
+            callback(null, 'Successfully opened a Pull Request' + (pr.html_url ? ' at ' + pr.html_url : ''));
+          })
         });
-      })
-    }).fail(function(xhr) {
-      callback(xhr.responseText);
-    })
-  }
-  window.open('https://github.com/login/oauth/authorize?' + $.param(params), '_blank');
+      });
+    });
+  });
 }
