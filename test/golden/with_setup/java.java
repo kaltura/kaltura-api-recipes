e9c2d48de4f55eeb6e7803352913a418cf8865d8
Bobby Brennan <bobby.brennan@gmail.com>
2017-01-16 20:23:12 +0000

    update java test

--- test/golden/with_setup/java.java
+++ test/golden/with_setup/java.java
@@ -10,7 +10,6 @@ class CodeExample {
   public static void main(String[] args) {
     try {
       KalturaConfiguration config = new KalturaConfiguration();
-      //config.setPartnerId(YOUR_PARTNER_ID);
       config.setEndpoint("https://www.kaltura.com/");
       KalturaClient client = new KalturaClient(config);
       String session = client.getSessionService().start(
