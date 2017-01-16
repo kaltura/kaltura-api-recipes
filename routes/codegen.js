6c9f174464214a850a2985271dbeae9c60586f97
Bobby Brennan <bobby.brennan@gmail.com>
2017-01-16 16:49:07 +0000

    refactor codegen route a bit

--- routes/codegen.js
+++ routes/codegen.js
@@ -2,25 +2,20 @@ var _ = require('lodash');
 var FS = require('fs');
 var Router = require('express').Router();
 var Lucy = require('lucy-codegen').Lucy;
-var KCode = require('kaltura-codegen'),
-    CodeTemplates = KCode.templates;
 var CodeTemplate = require('../codegen');
 
 module.exports = {};
 module.exports.initialize = function(cb) {
   require('../codegen/params')(function(renderParams) {
-    var LANGS = [
-      {name: 'php'},
-      {name: 'javascript'},
-      {name: 'node'},
-      {name: 'ruby'},
-    ]
-    LANGS.forEach(function(l) {
-      l.template = new CodeTemplate({language: l.name})
+    const codeTemplates = CodeTemplate.LANGUAGES.map(function(l) {
+      return {
+        name: l,
+        template: new CodeTemplate({language: l}),
+      };
     })
 
     Router.post('/code/build/request', function(req, res) {
-      var lang = LANGS.filter(l => l.name === req.body.language)[0];
+      var lang = codeTemplates.filter(l => l.name === req.body.language)[0];
       if (!lang) return res.status(500).send("Unknown language " + req.body.language);
       if (process.env.DEVELOPMENT) lang.template.reload();
       var path = req.body.request.path;
